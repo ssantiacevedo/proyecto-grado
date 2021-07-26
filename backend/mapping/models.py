ca0e60babc4255ca2a4060f7f5fdfec683e7adeb
Oscar Muñoz <osmunogue@gmail.com>
2021-07-26 20:30:42 -0300

    Los siguientes modelos fueron creados: Ontology, RelationalDB, MappingProcess. Tambien fueron creados los endpoints en donde se hara el alamacenamiento de los respectivos archivos.

--- backend/mapping/models.py
+++ backend/mapping/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Ontology(models.Model):
+    ontology_field = models.FileField(upload_to='ontologies/')
+
+class RelationalDB(models.Model):
+    relational_db_field = models.FileField(upload_to='relational_dbs/')
+
+class MappingProcess(models.Model):
+
+    ONTOLOGIES_ENTERED = 'ONTOS_ENT'
+    RELATIONAL_DB_ENTERED = 'DB_ENT'
+    MAPPING_DONE = 'MAP_DONE'
+
+    PROCESS_STATES = ((ONTOLOGIES_ENTERED, 'Ontologies entered'), 
+                        (RELATIONAL_DB_ENTERED, 'Relational DB entered'),
+                        (MAPPING_DONE, 'Mapping done'))
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    ontologies = models.ForeignKey(Ontology, on_delete=models.CASCADE)
+    relational_db = models.OneToOneField(RelationalDB, on_delete=models.CASCADE)
+    state = models.CharField(
+        choices=PROCESS_STATES,
+        default=ONTOLOGIES_ENTERED,
+        verbose_name='State of the mapping proccess',
+        max_length=10)
--- backend/mapping/urls.py
+++ backend/mapping/urls.py
@@ -1,7 +1,13 @@
+from django.conf.urls import url
 from django.urls import path
-from . import views
+from mapping.views import OntologyView, RelationalDBView
 
 urlpatterns = [
-    
+    path('ontologies/',
+        OntologyView.as_view(),
+        name='ontologies'),
+    path('relational_db/',
+        RelationalDBView.as_view(),
+        name='relational_db'),
 ]
 
