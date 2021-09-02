099cce6a02753db1d8b5bcf1c4632fea2c4cf110
Oscar Muñoz <osmunogue@gmail.com>
2021-09-02 00:20:46 -0300

    Now the ontology is generated by iterating according to the parameter steps_amount

--- backend/generator/utils.py
+++ backend/generator/utils.py
@@ -26,19 +26,19 @@ def generator(map_proccess, mapping):
                 list_mapped_elements.append(f'onto:{onto_name}')
 
     try:
-        # for _ in range(map_proccess.steps_amount):
-        res_n = graph.query(
-            f"PREFIX onto:<{onto.base_iri}> "
-            "CONSTRUCT {  ?mapped_object ?a ?b . ?x ?y ?mapped_object ; ?c ?d . ?d a ?e } "  
-            f"WHERE {{ VALUES ?mapped_object {{ {' '.join(list_mapped_elements)} }} ?mapped_object ?a ?b . ?x ?y ?mapped_object ; ?c ?d . ?d a ?e}} "   
-        )
-
-        with open(f"result-{map_proccess.uuid}.owl", "w") as owl_file:
-            owl_file.write(res_n.serialize(format='turtle').decode("utf-8"))
+        for _ in range(map_proccess.steps_amount):
+            res_n = graph.query(
+                f"PREFIX onto:<{onto.base_iri}> "
+                "CONSTRUCT {  ?mapped_object ?a ?b . ?x ?y ?mapped_object ; ?c ?d . ?d a ?e } "  
+                f"WHERE {{ VALUES ?mapped_object {{ {' '.join(list_mapped_elements)} }} ?mapped_object ?a ?b . ?x ?y ?mapped_object ; ?c ?d . ?d a ?e}} "   
+            )
             
-        # temp_onto = get_ontology(f"file://result-{map_proccess.uuid}.owl").load()
-        # onto_classes = list(temp_onto.classes())
-        # list_mapped_elements = [i.iri for i in onto_classes]
+            res_n.serialize(format='pretty-xml', destination=f"result-{map_proccess.uuid}.owl")
+            
+            temp_onto = get_ontology(f"file://result-{map_proccess.uuid}.owl").load()
+            onto_classes = list(temp_onto.classes())
+            for onto_elem in onto_classes:
+                list_mapped_elements.append(f'onto:{onto_elem.name}')
         return True 
     except:
         return False
