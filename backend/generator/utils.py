76c217abffd861aad74ae7dc2a44db826bd6638d
Oscar Muñoz <osmunogue@gmail.com>
2021-09-10 20:03:03 -0300

    Now multi ontologies mapping is allowed

--- backend/generator/utils.py
+++ backend/generator/utils.py
@@ -5,16 +5,21 @@ from owlready2 import *
 
 def generator(map_proccess):
 
-    ontology = map_proccess.ontologies.last()
-        
-    if ontology.ontology_type == 'FILE':
-        onto = get_ontology("file://media/" + ontology.ontology_file.name).load()
-    else:
-        onto = get_ontology(ontology.ontology_uri).load()
+    ontologies = map_proccess.ontologies.all()
+
+    load_ontologies = []
+    for ontology in ontologies:   
+        if ontology.ontology_type == 'FILE':
+            onto = get_ontology("file://media/" + ontology.ontology_file.name).load()
+        else:
+            onto = get_ontology(ontology.ontology_uri).load()
+        load_ontologies.append(onto)
 
     graph = default_world.as_rdflib_graph()
 
-    onto_classes = list(onto.classes())
+    onto_classes = []
+    for onto in load_ontologies:
+        onto_classes.extend(list(onto.classes()))
     classes = [i.iri for i in onto_classes]
 
     mapping = map_proccess.valid_mapping
@@ -22,19 +27,19 @@ def generator(map_proccess):
         return False
 
     list_mapped_elements = []
+
     for elem in mapping:
         db_elem = list(elem.keys())[0]
         onto_elems = elem[db_elem]
         for onto_elem in onto_elems:
             if onto_elem['iri'] in classes:
-                onto_name = onto_elem['name']
-                list_mapped_elements.append(f'onto:{onto_name}')
+                onto_name = onto_elem['iri']
+                list_mapped_elements.append(f'<{onto_name}>')
 
     try:
         for i in range(map_proccess.steps_amount):
             
             res_n = graph.query(
-                f"PREFIX onto:<{onto.base_iri}> "
                 "CONSTRUCT {  ?mapped_object ?a ?b . ?x ?y ?mapped_object ; ?c ?d . ?d a ?e } "  
                 f"WHERE {{ VALUES ?mapped_object {{ {' '.join(list_mapped_elements)} }} ?mapped_object ?a ?b . ?x ?y ?mapped_object ; ?c ?d . ?d a ?e}} "   
             )
@@ -44,9 +49,9 @@ def generator(map_proccess):
             temp_onto = get_ontology(f"file://media/result-{map_proccess.uuid}-{i}.owl").load()
             onto_classes = list(temp_onto.classes())
             for onto_elem in onto_classes:
-                elem_name = f"onto:{onto_elem.name}"
+                elem_name = f"<{onto_elem.iri}>"
                 if elem_name not in list_mapped_elements:
-                    list_mapped_elements.append(f"onto:{onto_elem.name}")
+                    list_mapped_elements.append(f"<{onto_elem.iri}>")
             if (i < map_proccess.steps_amount-1) and (os.path.exists(f"media/result-{map_proccess.uuid}-{i}.owl")):
                 os.remove(f"media/result-{map_proccess.uuid}-{i}.owl")
         return True 
